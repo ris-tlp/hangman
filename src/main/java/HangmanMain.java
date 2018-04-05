12a8a441e0a517073730414c01797c6c6c05c698
Shaeq Khan <shaeqkhan@gmail.com>
2018-04-04 21:11:33 -0400

    resolves #7

--- src/main/java/HangmanMain.java
+++ src/main/java/HangmanMain.java
@@ -12,4 +12,42 @@ public class HangmanMain {
     static int anotherCall() {
         return 47;
     }
+
+    static boolean isInputValid( String word, String reenterWord ) {
+
+        if( !word.equalsIgnoreCase(reenterWord) ) {
+            System.out.println("The words entered are not the same");
+            return false;
+        }
+        if( word.length() < 3 || word.length() > 10 ) {
+            System.out.println("The word be between 3 to 10 characters long.");
+            return false;
+        }
+        return isWordValid( word );
+
+    }
+
+    static boolean isWordValid( String word ) {
+
+        char[] lettersInWord = word.toCharArray();
+
+        for(int i = 0; i < lettersInWord.length; i++) {
+            if( !isCharacterValid(lettersInWord[i]) )
+                return false;
+        }
+
+        return true;
+
+    }
+
+    static boolean isCharacterValid( char c ) {
+
+        if( c >= 'a' && c <= 'z')
+            return true;
+        else if ( c >= 'A' && c <= 'Z' )
+            return true;
+        else
+            return false;
+
+    }
 }
--- src/test/java/HangmanMainTest.java
+++ src/test/java/HangmanMainTest.java
@@ -34,4 +34,54 @@ public class HangmanMainTest {
         int result = HangmanMain.anotherCall();
         Assert.assertEquals(47,result);
     }
+
+    @Test
+    public void test_isCharacterValid_True() {
+
+        String alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] allowedCharacters = alphabets.toCharArray();
+
+        for(int i=0; i < allowedCharacters.length; i++) {
+            Assert.assertTrue(HangmanMain.isCharacterValid(allowedCharacters[i]));
+        }
+
+    }
+
+    @Test
+    public void test_isInputCharacterValid_False() {
+
+        String nonAlphabeticCharacters = "0123456789`~!@#$%^&*()-=_+[]\\{}|;':,./<>?\"";
+        char[] disallowedCharacters = nonAlphabeticCharacters.toCharArray();
+
+        for(int i=0; i < disallowedCharacters.length; i++) {
+            Assert.assertFalse(HangmanMain.isCharacterValid(disallowedCharacters[i]));
+        }
+    }
+
+    @Test
+    public void test_isWordValid_True() {
+        String apple = "Apple";
+        Assert.assertTrue(HangmanMain.isWordValid(apple));
+    }
+
+    @Test
+    public void test_isWordValid_False() {
+        String apple = "Ap!le";
+        Assert.assertFalse(HangmanMain.isWordValid(apple));
+    }
+
+    @Test
+    public void test_isInputValid_True() {
+        String word = "Banana";
+        String reenterWord = "Banana";
+        Assert.assertTrue(HangmanMain.isInputValid(word, reenterWord));
+    }
+
+    @Test
+    public void test_isInputValid_False() {
+        String word = "Banana";
+        String reenterWord = "Banan";
+        Assert.assertFalse(HangmanMain.isInputValid(word, reenterWord));
+    }
+
 }
