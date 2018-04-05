26b38e34be3aa7564ffa7d706076ed992a7e66de
Shaeq Khan <shaeqkhan@gmail.com>
2018-04-04 21:32:06 -0400

    removed placeholder skeleton

--- src/main/java/HangmanMain.java
+++ src/main/java/HangmanMain.java
@@ -1,16 +1,7 @@
 public class HangmanMain {
+
     public static void main(String[] args) {
-        String result = doSomething("arg");
-        System.out.println("Test");
-        int i = anotherCall();
-    }
 
-    static String doSomething(String s) {
-        return "Function call 1 with " + s;
-    }
-
-    static int anotherCall() {
-        return 47;
     }
 
     static boolean isInputValid( String word, String reenterWord ) {
--- src/test/java/HangmanMainTest.java
+++ src/test/java/HangmanMainTest.java
@@ -22,18 +22,6 @@ public class HangmanMainTest {
 
     }
 
-    @Test
-    public void test_doSomething() {
-        String result = HangmanMain.doSomething("test");
-        Assert.assertEquals("Function call 1 with test", result);
-    }
-
-    //write test for anotherCall
-    @Test
-    public void test_anotherCall() {
-        int result = HangmanMain.anotherCall();
-        Assert.assertEquals(47,result);
-    }
 
     @Test
     public void test_isCharacterValid_True() {
