d50933b747e0f8b3deb2a969cec4e94deda4d5d5
Omar Pervez Khan <omarkhantlp@gmail.com>
2018-04-07 02:19:47 +0300

    removed unnecessary print lines

--- src/main/java/HangmanMain.java
+++ src/main/java/HangmanMain.java
@@ -13,8 +13,8 @@ public class HangmanMain {
         int validTurns = 6;
 
         String solution = word.toLowerCase();
-        System.out.println(solution);
-
+//        System.out.println(solution);
+//
         int length = solution.length();
 
         String guessSoFar = "";
@@ -48,13 +48,13 @@ public class HangmanMain {
         // checking if the string solution contains the character input by player two
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
 
-            System.out.println(guessSoFar + " guess so far");
+//            System.out.println(guessSoFar + " guess so far");
 
             if (solution.indexOf(P2input) >= 0) {
 
 //                System.out.println(guessSoFar);
 //                System.out.println(P2input);
-                System.out.println(guessSoFar.indexOf(P2input) + " index of p2input in guesssofar");
+//                System.out.println(guessSoFar.indexOf(P2input) + " index of p2input in guesssofar");
 
                 while (guessSoFar.toLowerCase().indexOf(P2input) >= 0) {
 
